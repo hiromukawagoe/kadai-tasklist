b048ff64118905323bf4874257e79fd6e0ee5ba9
hiromu.kawagoe <hiromukawagoe27@outlook.jp>
2021-03-24 11:16:59 +0000

    i should finish

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2021_03_23_074406) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -25,9 +27,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_074406) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_users_on_user_id"
   end
 
-  add_foreign_key "users", "users"
+  add_foreign_key "tasks", "users"
 end
